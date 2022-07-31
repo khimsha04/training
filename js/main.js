9460bb8a5a69d181603635409dc367f73658b53d
zuka <64226929+uknown-me@users.noreply.github.com>
2022-07-31 18:27:35 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,94 +1,94 @@
 const board = [
-  [1, 2, 3, 4],
-  [5, 6, 7, 8],
-  [9, 10, 11, 12],
-  [13, 14, 0, 15],
+    [1, 14, 3, 6],
+    [5, 4, 7, 12],
+    [9, 10, 11, 8],
+    [13, 2, 0, 15],
 ];
 
 const clickAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-77317/zapsplat_multimedia_button_click_fast_short_004_79288.mp3"
+    "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-77317/zapsplat_multimedia_button_click_fast_short_004_79288.mp3"
 );
 const wrongClickAudio = new Audio(
-  "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-77317/zapsplat_multimedia_button_click_fast_short_004_79288.mp3"
+    "https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-77317/zapsplat_multimedia_button_click_fast_short_004_79288.mp3"
 );
 
 
 for (let i = 0; i < board.length; i++) {
-  for (let j = 0; j < board[i].length; j++) {
-    if (board[i][j] === 0) continue;
+    for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] === 0) continue;
 
-    const div = document.createElement("div");
-    div.innerText = board[i][j];
-    div.style.top = i * 125 + "px";
-    div.style.left = j * 125 + "px";
-    div.dataset.i = i;
-    div.dataset.j = j;
-    document.getElementById("board").appendChild(div);
+        const div = document.createElement("div");
+        div.innerText = board[i][j];
+        div.style.top = i * 125 + "px";
+        div.style.left = j * 125 + "px";
+        div.dataset.i = i;
+        div.dataset.j = j;
+        document.getElementById("board").appendChild(div);
 
-    div.addEventListener("click", tryToMove);
-  }
+        div.addEventListener("click", tryToMove);
+    }
 }
 
 function tryToMove() {
-  const i = Number(this.dataset.i);
-  const j = Number(this.dataset.j);
+    const i = Number(this.dataset.i);
+    const j = Number(this.dataset.j);
 
-  if (board[i + 1] && board[i + 1][j] === 0) {
-    moveTo(this, i + 1, j);
-  } else if (board[i - 1] && board[i - 1][j] === 0) {
-    moveTo(this, i - 1, j);
-  } else if (board[i][j + 1] === 0) {
-    moveTo(this, i, j + 1);
-  } else if (board[i][j - 1] === 0) {
-    moveTo(this, i, j - 1);
-  } else {
-    clickAudio.play();
-  }
+    if (board[i + 1] && board[i + 1][j] === 0) {
+        moveTo(this, i + 1, j);
+    } else if (board[i - 1] && board[i - 1][j] === 0) {
+        moveTo(this, i - 1, j);
+    } else if (board[i][j + 1] === 0) {
+        moveTo(this, i, j + 1);
+    } else if (board[i][j - 1] === 0) {
+        moveTo(this, i, j - 1);
+    } else {
+        clickAudio.play();
+    }
 }
 
 function moveTo(element, i, j) {
-  board[i][j] = Number(element.innerText);
-  board[element.dataset.i][element.dataset.j] = 0;
-  element.style.top = i * 125 + "px";
-  element.style.left = j * 125 + "px";
-  element.dataset.i = i;
-  element.dataset.j = j;
+    board[i][j] = Number(element.innerText);
+    board[element.dataset.i][element.dataset.j] = 0;
+    element.style.top = i * 125 + "px";
+    element.style.left = j * 125 + "px";
+    element.dataset.i = i;
+    element.dataset.j = j;
 
-  checkIfFinished();
+    checkIfFinished();
 
-  clickAudio.play();
+    clickAudio.play();
 }
 
 function checkIfFinished() {
-  if (board.join() === "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0") {
-    clearInterval(timerInterval);
+    if (board.join() === "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0") {
+        clearInterval(timerInterval);
 
-    setTimeout(function () {
-      document.getElementById("won").innerHTML = "<h1>YOU WON!</h1>";
-      removeEventListeners();
-    }, 300);
-  }
+        setTimeout(function () {
+            document.getElementById("won").innerHTML = "<h1>YOU WON!</h1>";
+            removeEventListeners();
+        }, 300);
+    }
 }
 
 function reduceTime() {
-  let timeLeft = Number(document.getElementById("timer").innerText);
-  timeLeft--;
-  document.getElementById("timer").innerText = timeLeft;
+    let timeLeft = Number(document.getElementById("timer").innerText);
+    timeLeft--;
+    document.getElementById("timer").innerText = timeLeft;
 
-  if (timeLeft === 0) {
-    clearInterval(timerInterval);
-    document.getElementById("won").innerHTML = "<h1>Game over!</h1>";
-    removeEventListeners();
-  } else if (timeLeft <= 10) {
-    document.getElementById("timer").style.color = "red";
-  }
+    if (timeLeft === 0) {
+        clearInterval(timerInterval);
+        document.getElementById("won").innerHTML = "<h1>Game over!</h1>";
+        removeEventListeners();
+    } else if (timeLeft <= 10) {
+        document.getElementById("timer").style.color = "red";
+    }
 }
 
 const timerInterval = setInterval(reduceTime, 1000);
 
 function removeEventListeners() {
-  const nodes = document.querySelectorAll("#board div");
-  for (node of nodes) {
-    node.removeEventListener("click", tryToMove);
-  }
+    const nodes = document.querySelectorAll("#board div");
+    for (node of nodes) {
+        node.removeEventListener("click", tryToMove);
+    }
 }
