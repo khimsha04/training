c0f58fe93ad8b4f856559faffb6332b2fe7fd531
zuka <64226929+uknown-me@users.noreply.github.com>
2022-07-31 21:49:52 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ const wrongClickAudio = new Audio(
 for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i].length; j++) {
         if (board[i][j] === 0) continue;
-         if (board[i][j] === 15) continue;
+         
 
         const div = document.createElement("div");
         div.innerText = board[i][j];
